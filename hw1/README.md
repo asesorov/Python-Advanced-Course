de6697d0f9c69e8fd0d42558cdbd2059b059e73b
Aleksandr Sesorov <crafter2022@gmail.com>
2024-02-20 16:18:53 +0300

    Add nl script with artifact

---
+++ hw1/README.md
@@ -0,0 +1,11 @@
+# Homework 1
+
+## Task 1.1
+Написать упрощенный вариант утилиты `nl` -- скрипт, который выдает в `stdout` пронумерованные строки из файла.
+Если файл не передан, то скрипт читает строки из `stdin`.
+
+Он должен работать так же, как `nl -b a`.
+
+Try: `python numlines.py [optional: input file path]`
+
+Example output: [Artifact link](artifacts/output.txt)
---
+++ hw1/numlines.py
@@ -0,0 +1,40 @@
+import sys
+import argparse
+
+
+def process_lines(input_stream, line_number=1):
+    for line in input_stream:
+        yield f"{line_number}\t{line}"
+        line_number += 1
+
+def cli_argument_parser():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('file',
+                        help='Path to RKNN_benchmark',
+                        nargs='?',
+                        type=argparse.FileType('r'),
+                        default=sys.stdin)
+    args = parser.parse_args()
+
+    return args
+
+
+def main():
+    args = cli_argument_parser()
+    try:
+        for numbered_line in process_lines(args.file):
+            sys.stdout.write(numbered_line)
+        print()
+    except KeyboardInterrupt:
+        return 0
+    except Exception as e:
+        print(f"An error occurred: {e}\n")
+        return 1
+    finally:
+        if args.file is not sys.stdin:
+            args.file.close()
+
+
+if __name__ == '__main__':
+    sys.exit(main() or 0)
